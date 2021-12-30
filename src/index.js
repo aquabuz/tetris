d7939f0fada810e42c8e4c31a970fea56a878883
EastHill <aquabuz@gmail.com>
2021-12-30 23:58:08 +0900

    direction 추가

--- src/index.js
+++ src/index.js
@@ -16,18 +16,16 @@ let tempMovingItem;
 
 const BLOCKS = {
     tree: [
-        [
-            [0,0], [0,1], [1,0], [1,1],
-        ],
-        [],
-        [],
-        [],
+        [[2,1], [0,1], [1,0], [1,1]],
+        [[1,2], [0,1], [1,0], [1,1]],
+        [[1,2], [0,1], [2,1], [1,1]],
+        [[2,1], [1,2], [1,0], [1,1]],
     ],
 }
 
 const movingItem = {
     type: "tree",
-    direction: 0,
+    direction: 3,
     top: 0,
     left: 0,
 }
@@ -58,11 +56,76 @@ function prependNewLine() {
     playground.prepend(li)
 }
 
-function renderBlocks() {
+function renderBlocks(moveType="") {
     const { type, direction, top, left } = tempMovingItem;
+    const movingBlocks = document.querySelectorAll(".moving");
 
-    BLOCKS[type][direction].forEach(block => {
-        const x = block[0];
-        const y = block[1];
-    });
+    movingBlocks.forEach(moving => {
+        moving.classList.remove(type, "moving");
+    })
+
+    BLOCKS[type][direction].some(block => {
+        const x = block[0] + left;
+        const y = block[1] + top;
+        const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
+        const inAvailable = checkEmpty(target);
+        if (inAvailable) {
+            target.classList.add(type, "moving")
+        } else {
+            tempMovingItem = { ...movingItem }
+            setTimeout(() => {
+                if (moveType === "top") {
+                    seizeBlock()
+                }
+                renderBlocks();
+            }, 0)
+            return true;
+        }
+    })
+    movingItem.left = left;
+    movingItem.top = top;
+    movingItem.direction = direction;
 }
+
+function seizeBlock() {
+    console.log("seize block");
+}
+
+function checkEmpty(target) {
+    if (!target) {
+        return false;
+    }
+    return true;
+}
+
+function moveBlock(moveType, amount) {
+    tempMovingItem[moveType] += amount;
+    renderBlocks(moveType)
+}
+
+function changeDirection() {
+    const direction = tempMovingItem.direction;
+    direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction += 1;
+    renderBlocks()
+}
+
+// event handling
+document.addEventListener("keydown", e => {
+    switch(e.keyCode) {
+        case 39:
+            moveBlock("left", 1);
+            break;
+        case 37:
+            moveBlock("left", -1);
+            break;
+        case 40:
+            moveBlock("top", 1);
+            break;
+        case 32:
+            changeDirection();
+            break;
+        default:
+            break;
+    }
+    // console.log(e);
+})
